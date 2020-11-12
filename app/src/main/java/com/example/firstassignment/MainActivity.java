c2e0f3e515b5458303f8bc937120f130de2706de
Tal-Nitzan <tal.nitzan91@gmail.com>
2020-11-12 17:59:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firstassignment/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.firstassignment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("pttt", "onStart");
+        Log.d("pttt", "Timer Started");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("pttt", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("pttt", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d("pttt", "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("pttt", "onStop");
+        super.onStop();
+        Log.d("pttt", "Timer Stopped");
+        //Stop Timer
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("pttt", "onDestroy");
+        super.onDestroy();
+    }
+}
