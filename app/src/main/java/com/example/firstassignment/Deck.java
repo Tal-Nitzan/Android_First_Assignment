01898465119898c2c2e47f1d519b31e435009225
Talni <Tal.nitzan91@gmail.com>
2020-11-21 08:58:04 +0200

    Fixed everything.

--- app/src/main/java/com/example/firstassignment/Deck.java
+++ app/src/main/java/com/example/firstassignment/Deck.java
@@ -1,30 +1,37 @@
 package com.example.firstassignment;
 
+import java.util.Random;
+
 public class Deck {
+    public static final int NUM_OF_SIGNS = 4;
+    public static final int NUM_OF_CARDS = 13;
+    public static final int TOTAL_NUM_CARDS = NUM_OF_CARDS*NUM_OF_SIGNS;
     private Card[][] cards;
     private int drawedCards;
 
     public Deck() {
-        cards = new Card[4][13];
+        cards = new Card[NUM_OF_SIGNS][NUM_OF_CARDS];
         initDeck();
         this.drawedCards = 0;
     }
 
-    void initDeck() {
-        for (int i = 0; i < cards.length; i++) {
-            for (int j = 0; j < cards[0].length; j++) { // 0=h 1=d
+    public void initDeck() {
+        for (int i = 0; i < NUM_OF_SIGNS; i++) {
+            for (int j = 0; j < NUM_OF_CARDS; j++) { // 0=h 1=d
                 cards[i][j] = new Card(j+2, Card.calculateSign(i));
             }
         }
         this.shuffle();
     }
-    void shuffle() {
-        int numOfSwaps = ((int)(Math.random() * 48)) + 52; // The swap will generate a number between 52..100.
+
+    public void shuffle() {
+        Random rand = new Random();
+        int numOfSwaps = rand.nextInt((TOTAL_NUM_CARDS) + 1) + TOTAL_NUM_CARDS; // The swap will generate a number between TOTAL_NUM_CARDS..(TOTAL_NUM_CARDS*2).
         for (int i=0 ; i<numOfSwaps ; i++) {
-            int cardSign1 = (int)(Math.random() * 4);
-            int cardValue1 = (int)(Math.random() * 12);
-            int cardSign2 = (int)(Math.random() * 4);
-            int cardValue2 = (int)(Math.random() * 12);
+            int cardSign1 = rand.nextInt(NUM_OF_SIGNS);
+            int cardValue1 = rand.nextInt(NUM_OF_CARDS);
+            int cardSign2 = rand.nextInt(NUM_OF_SIGNS);
+            int cardValue2 = rand.nextInt(NUM_OF_CARDS);
             Card temp = cards[cardSign1][cardValue1];
             cards[cardSign1][cardValue1] = cards[cardSign2][cardValue2];
             cards[cardSign2][cardValue2] = temp;
@@ -32,12 +39,11 @@ public class Deck {
     }
 
     public boolean isEmpty() {
-        return drawedCards == 52;
+        return drawedCards == (NUM_OF_CARDS*NUM_OF_SIGNS);
     }
-
     Card drawCard() {
         drawedCards++;
-        return cards[((drawedCards-1)/4)%4][(drawedCards-1)%13];
+        return cards[((drawedCards-1)/NUM_OF_SIGNS)%NUM_OF_SIGNS][(drawedCards-1)%NUM_OF_CARDS];
     }
 
 }
